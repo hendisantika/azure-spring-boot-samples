4baebe2d970140b8899e839e1f47955673559f22
hendisantika <hendisantika@yahoo.co.id>
2019-02-05 05:49:20 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/azurespringbootsamples/AzureSpringBootSamplesApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.azurespringbootsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringBootSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureSpringBootSamplesApplication.class, args);
+    }
+
+}
+

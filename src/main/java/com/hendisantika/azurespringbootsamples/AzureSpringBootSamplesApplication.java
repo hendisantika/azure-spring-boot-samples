cdfe4e42b2f9a204b15dba54e2636f4f5f6ddf6d
hendisantika <hendisantika@yahoo.co.id>
2019-02-05 06:21:46 +0700

    #5 - Add Modify Main Class & Modify User Entity

--- src/main/java/com/hendisantika/azurespringbootsamples/AzureSpringBootSamplesApplication.java
+++ src/main/java/com/hendisantika/azurespringbootsamples/AzureSpringBootSamplesApplication.java
@@ -1,13 +1,41 @@
 package com.hendisantika.azurespringbootsamples;
 
+import com.hendisantika.azurespringbootsamples.entity.User;
+import com.hendisantika.azurespringbootsamples.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.Assert;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class AzureSpringBootSamplesApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureSpringBootSamplesApplication.class);
+
+    @Autowired
+    private UserRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(AzureSpringBootSamplesApplication.class, args);
+    }
+
+    public void run(String... var1) throws Exception {
+        final User testUser = new User(1L, "Uzumaki", "Naruto", "Konohagakure");
+
+        repository.deleteAll();
+        repository.save(testUser);
+
+        final Optional<User> opResult = repository.findById(testUser.getId());
+        Assert.isTrue(opResult.isPresent(), "Cannot find user.");
+
+        final User result = opResult.get();
+        Assert.state(result.getFirstName().equals(testUser.getFirstName()), "query result firstName doesn't match!");
+        Assert.state(result.getLastName().equals(testUser.getLastName()), "query result lastName doesn't match!");
+
+        LOGGER.info("findOne in User collection get result: {}", result.toString());
     }
 
 }
--- src/main/java/com/hendisantika/azurespringbootsamples/entity/User.java
+++ src/main/java/com/hendisantika/azurespringbootsamples/entity/User.java
@@ -3,6 +3,8 @@ package com.hendisantika.azurespringbootsamples.entity;
 import com.microsoft.azure.spring.data.cosmosdb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,15 +18,13 @@ import lombok.Data;
 @Document(collection = "mycollection")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    private String id;
+    @Id
+    private Long id;
     private String firstName;
     private String lastName;
     private String address;
-
-    public User() {
-
-    }
 
     @Override
     public String toString() {
